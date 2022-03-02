bef61a8288d84c7b07b35c33a408438b86d5c6a3
Lynn Nguyen <AmandaN22@outlook.com>
2022-03-01 18:55:40 -0700

    Initial api calls setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import apiCalls from './apiCalls';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      films: []
+    }
+  }
+
+  componentDidMount = () => {
+    apiCalls.getData('films')
+    .then(data => {
+      this.setState({ films: data})
+    })
+    .catch(() => `error`)
+  }
+
+  render() {
+    return(
+      <main>
+        <p>hi</p>
+      </main>  
+    )
+  }
 }
 
 export default App;
---
+++ src/apiCalls.js
@@ -0,0 +1,18 @@
+const apiCalls = {
+  getData(path) {
+    return fetch(`https://ghibliapi.herokuapp.com/${path}`)
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      } else {
+        if (response.status < 500) {
+          throw `We're sorry, something went wrong. Either the page doesn't exist, or could not be found.`
+        } else {
+          throw `We're sorry, something went wrong with the server. Please try again later`
+        }
+      } 
+    })
+  }
+}
+
+export default apiCalls;
